e4fb7aa7f53a7ee2f2c539cc2d5eb32d30ca0d0b
Justin <69999075+Jumner@users.noreply.github.com>
2021-07-05 16:05:20 -0400

    Improved word counting
    Centralized events and made word counting more robust. 2 spaces no longer count for multiple words

--- main.js
+++ main.js
@@ -4,16 +4,22 @@ const clearBtn = document.getElementById("clearBtn");
 
 function countWord() {
   let text = textField.value;
-  text = text.trim();
-  const words = text.split(" ");
-  if (words[0] == "") {
-    wordCount.innerText = 0;
-  } else {
-    wordCount.innerText = words.length;
-  }
+	text = text.trim();
+	let words = text.split(/\s+/); // Use regex to filter out double spaces and other weirdness
+  wordCount.innerText = words.filter(word => { // Filter out "bad" words
+		return word; // Keep word if it evaluates to true (removes empty words)
+	}).length;
 }
 
 clearBtn.onclick = () => {
-  textField.value = "";
+  textField.value = ""; // Removes all text
   countWord();
 };
+
+textField.oninput = () => { // Keeping events in one place
+	countWord();
+};
+
+window.onload = () => { // Text is preserved during refresh so count words on start
+	countWord();
+}
